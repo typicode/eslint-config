74f859ad0195408110faf3df37829d4bf08939e5
typicode <typicode@gmail.com>
2021-03-05 04:50:13 +0100

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      { semi: false, singleQuote: true, trailingComma: 'all' },
+    ],
+  },
+  env: {
+    node: true,
+  },
+}
