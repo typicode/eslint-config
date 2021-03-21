ab8eed19d3303a2fb2cce077fee00748ec25e040
typicode <typicode@gmail.com>
2021-03-21 02:05:37 +0100

    feat: add eslint-plugin-simple-import-sort

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'simple-import-sort',
     'plugin:prettier/recommended',
   ],
   rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
     'prettier/prettier': [
       'error',
       { semi: false, singleQuote: true, trailingComma: 'all' },
